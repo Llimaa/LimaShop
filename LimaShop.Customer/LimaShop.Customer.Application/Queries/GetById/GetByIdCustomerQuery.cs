98503cdb5468b9b9baebec255312855dfa3ea3ab
Marcos_Lima <limaa45@outlook.com>
2022-02-25 19:56:18 -0300

    finish GetByIdCustomerQueryHandler

---
+++ LimaShop.Customer/LimaShop.Customer.Application/Queries/GetById/GetByIdCustomerQuery.cs
@@ -0,0 +1,15 @@
+﻿using LimaShop.Customer.Application.ViewModel;
+using MediatR;
+using System;
+
+namespace LimaShop.Customer.Application.Queries.GetById
+{
+    public class GetByIdCustomerQuery : IRequest<CustomerViewModel>
+    {
+        public GetByIdCustomerQuery(Guid id)
+        {
+            Id = id;
+        }
+        public Guid Id { get; set; }
+    }
+}
---
+++ LimaShop.Customer/LimaShop.Customer.Application/Queries/GetById/GetByIdCustomerQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using LimaShop.Customer.Application.ViewModel;
+using LimaShop.Customer.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LimaShop.Customer.Application.Queries.GetById
+{
+    public class GetByIdCustomerQueryHandler : IRequestHandler<GetByIdCustomerQuery, CustomerViewModel>
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public GetByIdCustomerQueryHandler(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public async Task<CustomerViewModel> Handle(GetByIdCustomerQuery request, CancellationToken cancellationToken)
+        {
+            var customer = await _customerRepository.GetByIdAsync(request.Id);
+            if (customer == null) return null;
+
+            var customerViewModel = new CustomerViewModel(
+              customer.Id,
+              customer.FullName,
+              customer.BirthDate,
+              customer.PhoneNumber,
+              AddressCustomerViewModel.FromEntity(customer.Address), customer.Email, customer.Active);
+            return customerViewModel;
+        }
+    }
+}
