7e8c477749d585c88e034e8e1a67f06dfee8b741
Marcos_Lima <limaa45@outlook.com>
2022-02-25 19:53:21 -0300

    finish UpdateAddressCustomerHandler

---
+++ LimaShop.Customer/LimaShop.Customer.Application/Commands/Handlers/UpdateAddressCustomerHandler.cs
@@ -0,0 +1,40 @@
+﻿using LimaShop.Customer.Application.Commands.InputModels;
+using LimaShop.Customer.Core.Repositories;
+using LimaShop.Customer.Core.ValueObjects;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LimaShop.Customer.Application.Commands.Handlers
+{
+    public class UpdateAddressCustomerHandler : IRequestHandler<UpdateAddressCustomer, Unit>
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public UpdateAddressCustomerHandler(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public async Task<Unit> Handle(UpdateAddressCustomer request, CancellationToken cancellationToken)
+        {
+            var customer = await _customerRepository.GetByIdAsync(request.CustomerId);
+
+            var address = new Address(
+                request.AddressCustomer.Country,
+                request.AddressCustomer.State,
+                request.AddressCustomer.City,
+                request.AddressCustomer.Longradouro,
+                request.AddressCustomer.Street,
+                request.AddressCustomer.Number,
+                request.AddressCustomer.ZipCore
+                );
+
+            customer.UpdateAddress(address);
+
+            await _customerRepository.Update(customer);
+            return Unit.Value;
+
+        }
+    }
+}
---
+++ LimaShop.Customer/LimaShop.Customer.Application/Commands/InputModels/UpdateAddressCustomer.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+
+namespace LimaShop.Customer.Application.Commands.InputModels
+{
+    public class UpdateAddressCustomer: IRequest<Unit>
+    {
+        public Guid CustomerId { get; set; }
+        public AddressCustomer AddressCustomer { get; set; }
+    }
+}
