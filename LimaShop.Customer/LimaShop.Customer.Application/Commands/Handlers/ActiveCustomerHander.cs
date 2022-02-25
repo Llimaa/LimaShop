395379c9c3521caa1f05b58a7a66de5a82c1cd6d
Marcos_Lima <limaa45@outlook.com>
2022-02-25 19:51:32 -0300

    finish ActiveCustomerHander

---
+++ LimaShop.Customer/LimaShop.Customer.Application/Commands/Handlers/ActiveCustomerHander.cs
@@ -0,0 +1,24 @@
+﻿using LimaShop.Customer.Application.Commands.InputModels;
+using LimaShop.Customer.Core.Repositories;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LimaShop.Customer.Application.Commands.Handlers
+{
+    public class ActiveCustomerHander : IRequestHandler<ActiveCustomer, Unit>
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public ActiveCustomerHander(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public async Task<Unit> Handle(ActiveCustomer request, CancellationToken cancellationToken)
+        {
+            await _customerRepository.ActiveCustomer(request.CustomerId);
+            return Unit.Value;
+        }
+    }
+}
---
+++ LimaShop.Customer/LimaShop.Customer.Application/Commands/InputModels/ActiveCustomer.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+namespace LimaShop.Customer.Application.Commands.InputModels
+{
+    public class ActiveCustomer : IRequest<Unit>
+    {
+        public ActiveCustomer(Guid customerId)
+        {
+            CustomerId = customerId;
+        }
+
+        public Guid CustomerId { get; set; }
+    }
+}
