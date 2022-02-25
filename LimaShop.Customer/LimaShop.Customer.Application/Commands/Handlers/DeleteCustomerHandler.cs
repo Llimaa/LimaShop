c5f2e310a65cc76affdad0c725ac678eb50e55af
Marcos_Lima <limaa45@outlook.com>
2022-02-25 19:52:31 -0300

    finish DeleteCustomerHandler

---
+++ LimaShop.Customer/LimaShop.Customer.Application/Commands/Handlers/DeleteCustomerHandler.cs
@@ -0,0 +1,24 @@
+﻿using LimaShop.Customer.Application.Commands.InputModels;
+using LimaShop.Customer.Core.Repositories;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LimaShop.Customer.Application.Commands.Handlers
+{
+    public class DeleteCustomerHandler : IRequestHandler<DeleteCustomer, bool>
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public DeleteCustomerHandler(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public async Task<bool> Handle(DeleteCustomer request, CancellationToken cancellationToken)
+        {
+            var isDeleted = await _customerRepository.DeleteById(request.CustomerId);
+            return isDeleted;
+        }
+    }
+}
---
+++ LimaShop.Customer/LimaShop.Customer.Application/Commands/InputModels/DeleteCustomer.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+
+namespace LimaShop.Customer.Application.Commands.InputModels
+{
+    public class DeleteCustomer: IRequest<bool>
+    {
+        public DeleteCustomer(Guid customerId)
+        {
+            CustomerId = customerId;
+        }
+
+        public Guid CustomerId { get; set; }
+    }
+}
