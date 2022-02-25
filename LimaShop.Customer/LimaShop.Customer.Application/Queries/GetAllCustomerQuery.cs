ac048d28eb7bd6c9de5f0083c264d211e2e6e75c
Marcos_Lima <limaa45@outlook.com>
2022-02-25 19:57:13 -0300

    finish GetAllCustomerQueryHandler

---
+++ LimaShop.Customer/LimaShop.Customer.Application/Queries/GetAllCustomerQuery.cs
@@ -0,0 +1,10 @@
+﻿using LimaShop.Customer.Application.ViewModel;
+using MediatR;
+using System.Collections.Generic;
+
+namespace LimaShop.Customer.Application.Queries
+{
+    public class GetAllCustomerQuery : IRequest<IEnumerable<CustomerViewModel>>
+    {
+    }
+}
---
+++ LimaShop.Customer/LimaShop.Customer.Application/Queries/GetAllCustomerQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using LimaShop.Customer.Application.ViewModel;
+using LimaShop.Customer.Core.Repositories;
+using MediatR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LimaShop.Customer.Application.Queries
+{
+    public class GetAllCustomerQueryHandler : IRequestHandler<GetAllCustomerQuery, IEnumerable<CustomerViewModel>>
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public GetAllCustomerQueryHandler(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public async Task<IEnumerable<CustomerViewModel>> Handle(GetAllCustomerQuery request, CancellationToken cancellationToken)
+        {
+            var customers = await _customerRepository.GetAllAsync();
+
+            if (customers == null) return null;
+
+            var customerViewModel = customers
+                .Select(x => new CustomerViewModel(x.Id, x.FullName, x.BirthDate, x.PhoneNumber, AddressCustomerViewModel.FromEntity(x.Address), x.Email, x.Active))
+                .ToList();
+            return customerViewModel;
+        }
+    }
+}
