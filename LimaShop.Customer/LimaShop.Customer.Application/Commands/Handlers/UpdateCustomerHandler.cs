0c67ba183adf8dca604b34c7af43173c054d7a3a
Marcos_Lima <limaa45@outlook.com>
2022-02-25 19:53:40 -0300

    finish UpdateCustomerHandler

---
+++ LimaShop.Customer/LimaShop.Customer.Application/Commands/Handlers/UpdateCustomerHandler.cs
@@ -0,0 +1,25 @@
+﻿using LimaShop.Customer.Application.Commands.InputModels;
+using LimaShop.Customer.Core.Repositories;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LimaShop.Customer.Application.Commands.Handlers
+{
+    public class UpdateCustomerHandler : IRequestHandler<UpdateCustomer, Unit>
+    {
+        private readonly ICustomerRepository _customerRepository;
+        public UpdateCustomerHandler(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public async Task<Unit> Handle(UpdateCustomer request, CancellationToken cancellationToken)
+        {
+            var customer = await _customerRepository.GetByIdAsync(request.CustomerId);
+            customer.updateCustomer(request.FullName, request.BirthDate, request.PhoneNumber, request.Email);
+            await _customerRepository.Update(customer);
+            return Unit.Value;
+        }
+    }
+}
---
+++ LimaShop.Customer/LimaShop.Customer.Application/Commands/InputModels/UpdateCustomer.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+
+namespace LimaShop.Customer.Application.Commands.InputModels
+{
+    public class UpdateCustomer : IRequest<Unit>
+    {
+        public Guid CustomerId { get; set; }
+        public string FullName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
