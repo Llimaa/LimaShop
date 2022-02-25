04f86a37152976fffc6f12e87083bc0ead79e402
Marcos_Lima <limaa45@outlook.com>
2022-02-25 19:52:53 -0300

    finish DesactiveCustomer Handler

---
+++ LimaShop.Customer/LimaShop.Customer.Application/Commands/Handlers/DesactiveCustomerHander.cs
@@ -0,0 +1,24 @@
+﻿using LimaShop.Customer.Application.Commands.InputModels;
+using LimaShop.Customer.Core.Repositories;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LimaShop.Customer.Application.Commands.Handlers
+{
+    public class DesactiveCustomerHander : IRequestHandler<DesactiveCustomer, Unit>
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public DesactiveCustomerHander(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public async Task<Unit> Handle(DesactiveCustomer request, CancellationToken cancellationToken)
+        {
+            await _customerRepository.DesactiveCustomer(request.CustomerId);
+            return Unit.Value;
+        }
+    }
+}
---
+++ LimaShop.Customer/LimaShop.Customer.Application/Commands/InputModels/DesactiveCustomer.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+
+namespace LimaShop.Customer.Application.Commands.InputModels
+{
+    public class DesactiveCustomer : IRequest<Unit>
+    {
+        public DesactiveCustomer(Guid customerId)
+        {
+            CustomerId = customerId;
+        }
+
+        public Guid CustomerId { get; set; }
+    }
+}
