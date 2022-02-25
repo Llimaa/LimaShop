5da09ff6b92fb91f952da81a2811049eb29e53a2
Marcos_Lima <limaa45@outlook.com>
2022-02-25 19:52:06 -0300

    finish CreateCustomer handler

---
+++ LimaShop.Customer/LimaShop.Customer.Application/Commands/Handlers/CreateCustomerHandler.cs
@@ -0,0 +1,27 @@
+﻿using LimaShop.Customer.Application.Commands.InputModels;
+using LimaShop.Customer.Core.Repositories;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LimaShop.Customer.Application.Commands.Handlers
+{
+    public class CreateCustomerHandler : IRequestHandler<CreateCustomer, Guid>
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CreateCustomerHandler(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public async Task<Guid> Handle(CreateCustomer request, CancellationToken cancellationToken)
+        {
+            var customer = request.ToEntity();
+
+            await _customerRepository.Insert(customer);
+            return customer.Id;
+        }
+    }
+}
---
+++ LimaShop.Customer/LimaShop.Customer.Application/Commands/InputModels/CreateCustomer.cs
@@ -0,0 +1,19 @@
+﻿using LimaShop.Customer.Core.Entities;
+using MediatR;
+using System;
+
+namespace LimaShop.Customer.Application.Commands.InputModels
+{
+    public class CreateCustomer : IRequest<Guid>
+    {
+        public string FullName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public AddressCustomer Address { get; set; }
+        public string Email { get; set; }
+        public CustomerEntity ToEntity()
+        {
+            return new CustomerEntity(FullName, BirthDate, PhoneNumber, Address.ToEntity(), Email);
+        }
+    }
+}
